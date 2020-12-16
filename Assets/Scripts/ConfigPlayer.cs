c8077def431766868ebc314fc73b39716e5034a7
RemiLecouillard <remi@lecouillard.org>
2020-12-16 09:57:38 +0100

    Adds player

--- Assets/Scripts/ConfigPlayer.cs
+++ Assets/Scripts/ConfigPlayer.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Config", order = 1)]
-public class ConfigPlayer : ScriptableObject
-{
-    [SerializeField]
-    private float _movementSpeed;
+namespace deflagration {
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Config", order = 1)]
+    public class ConfigPlayer : ScriptableObject
+    {
+        [SerializeField]
+        private float _movementSpeed;
 
-    public float getMovementSpeed => _movementSpeed;
+        public float getMovementSpeed => _movementSpeed;
 
-    [SerializeField]
-    private float _numberOfJump;
-    
-    public float getNumberOfJump => _numberOfJump;
+        [SerializeField]
+        private float _numberOfJump;
+        
+        public float getNumberOfJump => _numberOfJump;
 
-    [SerializeField]
-    private float _jumpForce;
-    
-    public float getJumpForce => _jumpForce;
+        [SerializeField]
+        private float _jumpForce;
+        
+        public float getJumpForce => _jumpForce;
 
+
+    }
 
 }
