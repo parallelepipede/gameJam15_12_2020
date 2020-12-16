a9a3bcec065c4a59b592b7bf1224400e0ceb52fd
Paul Zanoncelli <paul.zanoncelli@gmail.com>
2020-12-16 15:40:37 +0100

    big explosion behind player

---
+++ Assets/Scripts/explosionHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosionHit : MonoBehaviour
+{
+    private  Rigidbody rb;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(0,0,speed,ForceMode.Impulse);
+    }
+    
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //col.gameObject.GetComponent<Player>().killPlayer();
+            Debug.Log("touché");
+        }
+    
+    }
+}
